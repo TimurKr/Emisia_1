461abed611128f41a868484ac7975f04328a7728
Timur Kramár <timurjan.kramar123@gmail.com>
2022-04-20 16:20:55 +0200

    Editing to return 0 if any vertex lies on line

--- src/Emisia_1.c
+++ src/Emisia_1.c
@@ -4,42 +4,42 @@
 
 #include "stdio.h"
 
+#define ALMOST_ZERO 0.0000000001
+
 unsigned int uholnik_roztaty_priamkou_pocet_komponent(
         const float *x, const float *y, unsigned int l, float a, float b, float c) {
 
-    int prienik_counter = 0;                // počítadlo, koľko krát prenikne priamka cez okraj uholnika
-    unsigned short int last_pos = 0;        // -1 => pod priemkou, 1 => nad priamkov, 0 => na priamke
-    unsigned short int new_pos = 0;
+    int prienik_counter = 0;            // počítadlo, koľko krát prenikne priamka cez okraj uholnika
+    unsigned short int last_pos;        // -1 => pod priemkou, 1 => nad priamkov, 0 => na priamke
+    unsigned short int new_pos;
 
             // Zisti polohu posledného bodu ku priamke
     float diff = y[l-1] - (float) - (a*x[l-1] + c) / (float) b;
-    if (diff > 0) {
+    if (diff > ALMOST_ZERO) {
         last_pos = 1;
         printf("Poloha -1-veho je nad priamkou\n");
-    } else if (diff < 0) {
+    } else if (diff < ALMOST_ZERO) {
         last_pos = -1;
         printf("Poloha -1-veho je pod priamkou\n");
     } else {
-        last_pos = 0;
-        printf("Poloha -1-veho je na priamke\n");
+        return 0;
     }
 
     for (int i = 0; i < l; i++) {
                 // Zisti polohu i-teho bodu ku priamke
         diff = y[i] - (float) - (a*x[i] + c) / (float) b;
-        if (diff > 0) {
+        if (diff > ALMOST_ZERO) {
             new_pos = 1;
             printf("Poloha %d-teho je nad priamkou\n", i);
-        } else if (diff < 0) {
+        } else if (diff < ALMOST_ZERO) {
             new_pos = -1;
             printf("Poloha %d-teho je pod priamkou\n", i);
         } else {
-            new_pos = 0;
-            printf("Poloha %d-teho je na priamke\n", i);
+            return 0;
         }
 
                 // Zisti, či sa zmenila poloha, ak hej, musel byť prienik
-        if (new_pos != 0 && last_pos != 0 && new_pos != last_pos) {
+        if (new_pos != last_pos) {
             printf("Medzi bodom %d a %d je prienik\n", i-1, i);
             prienik_counter++;
         }
